fff79d42bf877bad3845818e151d2066a797b685
Cscott84 <45050662+Cscott84@users.noreply.github.com>
2020-03-10 18:23:07 -0400

    Add files via upload

---
+++ employee/Emp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Emp
+{
+
+    public string fName;
+
+    public string lName;
+
+    public int Id;
+
+    public double score;
+    
+    
+    public Emp(string firstName, string lastName, int ID, double Score)
+	{
+        fName = firstName;
+        lName = lastName;
+        Id = ID;
+        score = Score;
+	}
+
+    public void print()
+    {
+        Console.WriteLine($"Employee First Name: {fName}");
+        Console.WriteLine($"Employee Last name: {lName}");
+        Console.WriteLine($"Employee Identification number: {Id}");
+        Console.WriteLine($"Employee Evaluation Score: {score}");
+
+
+
+    }
+
+
+}
---
+++ employee/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please Enter Employee First Name: ");
+            String firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter Employee's Last Name: ");
+            String lastname = Console.ReadLine();
+
+            Console.WriteLine("Please Enter Employee Number: ");
+            int Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter evaluation score: ");
+            double eval = Convert.ToDouble(Console.ReadLine());
+
+            Emp Employee1 = new Emp(firstName,lastname,Id,eval);
+            Employee1.print();
+        }
+    }
+}
