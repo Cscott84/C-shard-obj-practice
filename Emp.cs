79c9f322772fe3e861efd42d111238abc60f31a4
Cscott84 <45050662+Cscott84@users.noreply.github.com>
2020-03-10 18:46:59 -0400

    Added validation methods for integer and double input

---
+++ Emp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public class Emp
+{
+
+    public string fName;
+
+    public string lName;
+
+    public int Id;
+
+    public double score;
+    
+    
+    public Emp(string firstName, string lastName, int ID, double Score)
+	{
+        fName = firstName;
+        lName = lastName;
+        Id = ID;
+        score = Score;
+	}
+
+    public static int validInt(string askForInt)
+    {
+        int result;
+        Console.WriteLine(askForInt);
+        string input1 = Console.ReadLine();
+
+        while(!Int32.TryParse(input1, out result))
+        {
+            Console.WriteLine("Invalid input, Please enter a numeric value: ");
+            Console.WriteLine(askForInt);
+            input1 = Console.ReadLine();
+
+        }
+
+        return result;
+    }
+
+
+    public static double validDouble(string askForDouble)
+    {
+        double result;
+        Console.WriteLine(askForDouble);
+        string input2 = Console.ReadLine();
+
+        while(!double.TryParse(input2, out result))
+        {
+            Console.WriteLine("Invalid input, Please enter numeric value: ");
+            Console.WriteLine(askForDouble);
+            input2 = Console.ReadLine();
+        }
+
+        return result;
+
+    }
+
+    public void print()
+    {
+        Console.WriteLine($"Employee First Name: {fName}");
+        Console.WriteLine($"Employee Last name: {lName}");
+        Console.WriteLine($"Employee Identification number: {Id}");
+        Console.WriteLine($"Employee Evaluation Score: {score}");
+
+
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please Enter Employee First Name: ");
+            String firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter Employee's Last Name: ");
+            String lastname = Console.ReadLine();
+
+            
+            int Id = Emp.validInt("Please Enter Employee identification number: ");
+            
+            double eval = Emp.validDouble("Please enter Employee Evaluation score: ");
+
+            Emp Employee1 = new Emp(firstName,lastname,Id,eval);
+            Employee1.print();
+        }
+    }
+}
