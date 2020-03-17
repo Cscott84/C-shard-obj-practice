3e00a8cdf37a482f3df05db012ad7f27c30a5318
Cscott84 <45050662+Cscott84@users.noreply.github.com>
2020-03-17 12:02:33 -0400

    added loop to allow user to add more employees

--- Program.cs
+++ Program.cs
@@ -21,6 +21,34 @@ namespace employee
 
             Emp Employee1 = new Emp(firstName,lastname,Id,eval);
             Employee1.print();
+
+
+            Console.WriteLine("Would you like to add another employee?");
+
+            string answer = Console.ReadLine();
+
+            while (answer == "yes" || answer == "Yes" || answer == "Y" || answer =="y")
+            {
+
+            Console.WriteLine("Please Enter Employee First Name: ");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter Employee's Last Name: ");
+            lastname = Console.ReadLine();
+
+            
+            Id = Emp.validInt("Please Enter Employee identification number: ");
+            
+             eval = Emp.validDouble("Please enter Employee Evaluation score: ");
+
+            Emp Employee2 = new Emp(firstName,lastname,Id,eval);
+            Employee1.print();
+            
+            Console.WriteLine("Would you like to add another employee?");
+
+            answer = Console.ReadLine();
+            
+            }
         }
     }
 }
